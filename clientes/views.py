3b0d2896b79936393074ff072ef9a51e118a6944
the-ppz <frank1995alvcr@gmail.com>
2023-12-20 19:37:35 -0500

    Login

--- clientes/views.py
+++ clientes/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import *
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class Inicio(TemplateView):
+class Inicio(LoginRequiredMixin, TemplateView):
     template_name = 'crud/home.html' 
+    login_url = '/admin'
     
-class Compras(TemplateView):
+class Compras(LoginRequiredMixin, TemplateView):
     template_name = 'crud/compras.html' 
+    login_url = '/admin'
